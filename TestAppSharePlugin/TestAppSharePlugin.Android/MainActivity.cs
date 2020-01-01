8309a40fb845970eaf5aae425efeb3af092b528c
Ravi <rvsngh87@gmail.com>
2020-01-01 15:31:23 +0530

    first commit

---
+++ TestAppSharePlugin/TestAppSharePlugin.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Permissions;
+using Xamarin.Forms;
+using TestAppSharePlugin.Droid.Services;
+using Plugin.CurrentActivity;
+
+namespace TestAppSharePlugin.Droid
+{
+    [Activity(Label = "TestAppSharePlugin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            CrossCurrentActivity.Current.Init(this, bundle);
+            LoadApplication(new App());
+            DependencyService.Register<ScreenshotService>();
+            DependencyService.Get<ScreenshotService>().SetActivity(this);
+            DependencyService.Register<ShareService>();
+        }
+    }
+}
+
---
+++ TestAppSharePlugin/TestAppSharePlugin/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAppSharePlugin.Interfaces;
+using Xamarin.Forms;
+
+namespace TestAppSharePlugin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var screenshot = DependencyService.Get<IScreenshotService>().Capture();
+            MemoryStream ms = new MemoryStream(screenshot);
+            ImageSource imageSource = ImageSource.FromStream(() => { return ms; });
+            //*********** Here title and message could be dynamic as per your requirements ***********
+            DependencyService.Get<IShareService>().Share("Test Title","Test Message", imageSource);
+        }
+    }
+}
